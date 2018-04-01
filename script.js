27ce87f218c47183b5746cdd806bfae88b39cb68
angusbayley <angusbayley@gmail.com>
2018-04-01 18:06:50 +0100

    visual indicator text for toggling sound

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ window.onload = function() {
 addVideoClickListener = function() {
     video = document.getElementById('video')
     video.addEventListener("click", toggleAudio)
+
+    video.onmouseover = function(){showText()}
+    video.onmouseout = function(){hideText()}
 }
 
 toggleAudio = function(e) {
     e.target.muted = !e.target.muted
+}
+
+showText = function() {
+    toggleText = document.getElementById('toggle-text')
+    toggleText.style.visibility = 'visible'
+}
+
+hideText = function() {
+    toggleText = document.getElementById('toggle-text')
+    toggleText.style.visibility = 'hidden'
 }
